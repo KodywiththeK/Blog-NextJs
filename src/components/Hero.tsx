1f24b7ef1896c3748f121f614eb00cfad97a9da5
KodywiththeK <jmsds.km@gmail.com>
2023-06-05 17:17:29 +0900

    about

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import profileImage from '../../public/images/profileImage.jpg'
 
@@ -9,7 +10,9 @@ export default function Hero() {
       <h2 className="mt-2 text-3xl font-bold">{"Hi, I'm Kody"}</h2>
       <h3 className="text-xl font-semibold">Frontend Engineer</h3>
       <p>일상 속 스미는, 선한 영향력을 만드는 사람</p>
-      <button className="my-1 mt-2 rounded-xl bg-yellow-500 px-4 py-2 font-bold">Contact me</button>
+      <Link href={'/contact'}>
+        <button className="my-1 mt-2 rounded-xl bg-yellow-500 px-4 py-2 font-bold">Contact me</button>
+      </Link>
     </section>
   )
 }
